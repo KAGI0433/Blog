42a2336c1af55c2f95e7802ef32b7210f50c6842
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-09-26 16:24:47 +0200

    frontend fetching of api

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
+
+  const [backendData, setbackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setbackendData(data)
+      }
+    )
+  }, [])
+
+
   return (
     <div>
 
